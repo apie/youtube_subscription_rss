e4e045fd53f1ee81f0e6a486b12481dd7fc52649
Denick <1049485+apie@users.noreply.github.com>
2023-01-21 15:11:55 +0100

    pep8 + timeout

--- generate_rss_for_channels.py
+++ generate_rss_for_channels.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-#Read channels.txt and get RSS for each.
-#Generate one single RSS for all channels combined.
-#By Apie 2021-11-22
+# Read channels.txt and get RSS for each.
+# Generate one single RSS for all channels combined.
+# By Apie 2021-11-22
 import requests
 import feedparser
 import dateparser
@@ -22,11 +22,11 @@ session = requests.Session()
 def yield_channel_entry(channel_url):
     if '/user/' in channel_url:
         username = channel_url.split('/user/')[1]
-        rss_url = RSS_BASE_USER+username
+        rss_url = RSS_BASE_USER + username
     else:
         channel_id = channel_url.split('/channel/')[1]
-        rss_url = RSS_BASE_CHANNEL+channel_id
-    response = session.get(channel_url)
+        rss_url = RSS_BASE_CHANNEL + channel_id
+    response = session.get(channel_url, timeout=3)
     response.raise_for_status()
     feed = feedparser.parse(rss_url)
     for i, entry in enumerate(feed.entries):  # Sorted on publish date
@@ -34,14 +34,16 @@ def yield_channel_entry(channel_url):
         if entry['published_datetime'] < TWO_YEARS_AGO:
             if i == 0:
                 print(f"Skipped {channel_url!r}")
-                return # Skip whole channel since first (newest) item is too old.
+                return  # Skip whole channel since first (newest) item is too old.
             else:
-                continue # Skip just this entry
+                continue  # Skip just this entry
         yield entry
+
 
 def get_items(channels):
     for channel in channels:
         yield from yield_channel_entry(channel.strip())
+
 
 def write_complete_rss_for_channels(channels, filename):
     feed = feedgenerator.Rss201rev2Feed(
@@ -59,6 +61,7 @@ def write_complete_rss_for_channels(channels, filename):
         )
     with open(path.join(path.dirname(path.realpath(argv[0])), filename), 'w') as f:
         feed.write(f, encoding='utf-8')
+
 
 if __name__ == '__main__':
     with open(path.join(path.dirname(path.realpath(argv[0])), 'channels.txt')) as c:
--- get_channels.py
+++ get_channels.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 # Gets user or channel link from your subscription page
-#By Apie 2021-11-22
+# By Apie 2021-11-22
 
 # URL = 'https://www.youtube.com/feed/channels'
-#TODO automatic retrieve, for now use a downloaded HTML file.
+# TODO automatic retrieve, for now use a downloaded HTML file.
 from lxml import html
+
 
 def get_channels():
     with open('Channel list - YouTube.html') as html_file:
         page = html.fromstring(html_file.read())
     return page.xpath("//a[@id='main-link']/@href")
+
 
 if __name__ == '__main__':
     for c in get_channels():
